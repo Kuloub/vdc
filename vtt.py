df9b626db9c5dafc829a6b19a3f149a6b5c7557c
Kuloub <153790525+Kuloub@users.noreply.github.com>
2024-01-26 10:34:52 -0800

    Update vtt.py

--- vtt.py
+++ vtt.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localllhoou88s",
   user="myuser4",
-  password="myasd",
+  password="myas",
   database="mydataase"
     user="myusernam214e",
 )
